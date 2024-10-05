7e0f43c2e427b7e1794cfcd17ae05527b0197203
tedarsakib777 <tedarsakib777@gmail.com>
2024-10-05 09:01:09 +0600

    Update info.js

--- scripts/commands/info.js
+++ scripts/commands/info.js
@@ -9,7 +9,7 @@ module.exports.config = {
     usages: "",
     cooldowns: 5,
     dependencies: 
-	{
+    {
     "request":"",
     "fs-extra":"",
     "axios":""
@@ -20,27 +20,40 @@ const axios = global.nodemodule["axios"];
 const request = global.nodemodule["request"];
 const fs = global.nodemodule["fs-extra"];
 const time = process.uptime(),
-		hours = Math.floor(time / (60 * 60)),
-		minutes = Math.floor((time % (60 * 60)) / 60),
-		seconds = Math.floor(time % 60);
+        hours = Math.floor(time / (60 * 60)),
+        minutes = Math.floor((time % (60 * 60)) / 60),
+        seconds = Math.floor(time % 60);
 const moment = require("moment-timezone");
 var juswa = moment.tz("Asia/Dhaka").format("『D/MM/YYYY』 【hh:mm:ss】");
-  
-var callback = () => api.sendMessage({body:`
---------------------------------------------
-𝐍𝐚𝐦𝐞       : 𝐌𝐨𝐡𝐚𝐦𝐦𝐚𝐝 𝐍𝐚𝐲𝐚𝐧
-𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 : 𝐌𝐨𝐡𝐚𝐦𝐦𝐚𝐝 𝐍𝐚𝐲𝐚𝐧
-𝐑𝐞𝐥𝐢𝐠𝐢𝐨𝐧   : 𝐈𝐬𝐥𝐚𝐦
-𝐏𝐞𝐫𝐦𝐚𝐧𝐞𝐧𝐭 𝐀𝐝𝐝𝐫𝐞𝐬𝐬: 𝐓𝐚𝐧𝐠𝐚𝐢𝐥, 𝐃𝐡𝐚𝐤𝐚
-𝐂𝐮𝐫𝐫𝐞𝐧𝐭 𝐀𝐝𝐝𝐫𝐞𝐬𝐬: 𝐌𝐨𝐲𝐦𝐨𝐧𝐬𝐢𝐧𝐡, 𝐃𝐡𝐚𝐤𝐚 𝐁𝐲𝐩𝐚𝐬𝐬
-𝐆𝐞𝐧𝐝𝐞𝐫.   : 𝐌𝐚𝐥𝐞
-𝐀𝐠𝐞           : 𝟏𝟖+
-𝐑𝐞𝐥𝐚𝐭𝐢𝐨𝐧𝐬𝐡𝐢𝐩 : 𝐒𝐢𝐧𝐠𝐥𝐞
-𝐖𝐨𝐫𝐤        : 𝐒𝐭𝐮𝐝𝐞𝐧𝐭
-𝐆𝐦𝐚𝐢𝐥       : mohammadnayan447@gmail.com
-𝐖𝐡𝐚𝐭𝐬𝐀𝐩𝐩: wa.me/+8801615298449
-𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦  : t.me/MOHAMMADNAYAN
-𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐋𝐢𝐧𝐤 : https://www.facebook.com/www.xnxx.com169`,attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID, () => 
+
+var callback = () => api.sendMessage({body:`     ⊰᯽⊱┈──╌❊╌──┈⊰᯽⊱
+۞ 𝗕𝗢𝗧 𝗔𝗗𝗠𝗜𝗡 𝗜𝗡𝗙𝗢 ۞
+◎ ══════ ❈ ══════ ◎
+╭─╮ 
+  🔥 
+╰─╯𝗡𝗔𝗠𝗘 👉𝗦𝗔𝗞𝗜𝗕 𝗠𝗔𝗛𝗠𝗨𝗗
+╭─╮ 
+  🔥 
+╰─╯𝗔𝗗𝗗𝗥𝗘𝗦𝗦👉𝗞𝗢𝗟𝗞𝗔𝗧𝗔
+╭─╮ 
+  🔥 
+╰─╯𝗪𝗛𝗔𝗧𝗦,𝗔𝗣𝗣👉01310762595
+╭─╮ 
+  🔥 
+╰─╯𝗥𝗘𝗟𝗔𝗧𝗜𝗢𝗡👉𝗦𝗜𝗡𝗚𝗘𝗟
+
+   BOT OWNER FB ID
+[ANY HELP CONTACT FB]
+
+ ➷➷➷➷➷➷➷➷➷➷➷➷➷➷ https://www.facebook.com/SAKIB.MAHMUD.V1
+
+⊰᯽⊱┈──╌❊╌──┈⊰᯽⊱
+┏•━•━•━ ◎ ━•━•━•┓
+ ❂ 𝗕𝗢𝗧  𝗢𝗪𝗡𝗘𝗥 ❂
+┗•━•━•━ ◎ ━•━•━•┛
+╭────────────╮
+✜ 𝗦𝗔𝗞𝗜𝗕 𝗠𝗔𝗛𝗠𝗨𝗗 ✜
+╰────────────╯`,attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID, () => 
     fs.unlinkSync(__dirname + "/cache/1.png"));  
       return request(encodeURI(`https://graph.facebook.com/100000959749712/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`)).pipe(
 fs.createWriteStream(__dirname+'/cache/1.png')).on('close',() => callback());
